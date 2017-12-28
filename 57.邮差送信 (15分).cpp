4c6f517a96ace14459cfaaf13b44cf4faaaf577a
hushhw <hushhw@sina.cn>
2017-12-28 15:12:47 +0800

    add

--- 57.邮差送信 (15分).cpp
+++ 57.邮差送信 (15分).cpp
@@ -21,3 +21,76 @@
 // 
 // 	输出样例
 // 	2
+
+//经典的dfs题
+//解析详见：http://blog.csdn.net/zlambert/article/details/65695000
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int n,k,x,y;
+int far[200];		//求路径，即存储它前一个结点
+int m=0,s=0;
+bool a[200][200];	//存路径
+bool vis[200];		//标记
+
+int print(int a)
+{
+	if(a==0)
+		return s;
+	else
+		s++;
+	//cout<<"a:"<<a<<endl;
+	//cout<<"f[a]:"<<far[a]<<endl;
+	print(far[a]);
+}
+
+void dfs()
+{
+	int q[11000], head=0, tail=1;	//q模拟队列
+	q[tail] = x;					//将第一个位置入队
+	far[x]=0;
+	vis[x]=1;			//标记为已访问
+	while(head < tail)	//如果队列非空，则移出队列首部状态，扩展他所有的子状态
+	{
+		head++;			//移出首部元素
+		for(int i=1; i<=n; ++i)
+		{
+			if(a[q[head]][i]==1 && vis[i]==0)	//如果有联通并且未访问
+			{
+				tail++;
+				q[tail]=i;		//满足条件则把该元素存入队列
+				far[i]=q[head];	//far指向队首
+				//cout<<"far:"<<far[i]<<endl;
+				//cout<<"q:"<<q[tail]<<endl;
+				vis[i]=1;		//标记为已访问
+				if(q[tail]==y)
+				{
+					cout<<print(q[tail])-2<<endl;
+					m=1;
+					return ;
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	memset(a,0,sizeof(a));
+	memset(vis,0,sizeof(vis));
+	memset(far,0,sizeof(far));
+	int b,c;
+	cin>>n>>k;
+	for(int i=1; i<=k; ++i)
+	{
+		cin>>b>>c;
+		a[b][c]=1;
+		a[c][b]=1;
+	}
+	cin>>x>>y;
+	dfs();
+	//system("pause");
+	return 0;
+}
